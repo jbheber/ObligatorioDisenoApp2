528fd23e3bf4f62ebca0c1dce09057c0728c78ea
Juan Bautista Heber Leborgne <Juan Bautista Heber Leborgne>
2016-05-10 15:51:19 -0300

    Se implementa la logica y se pasan las pruebas

---
+++ Stockapp/Stockapp.Logic/API/IPlayerLogic.cs
@@ -0,0 +1,23 @@
+﻿using Stockapp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockapp.Logic.API
+{
+    public interface IPlayerLogic
+    {
+        bool RegisterPlayer(Player player);
+
+        bool DeletePlayer(Player player);
+
+        bool DeletePlayer(Guid player);
+
+        bool UpdatePlayer(Player player);
+
+        Player GetPlayer(Guid playerId);
+
+    }
+}
