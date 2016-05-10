9ed99e11848fb5f9b74a92e7190436946cc0b966
Juan Bautista Heber Leborgne <Juan Bautista Heber Leborgne>
2016-05-10 10:08:16 -0300

    Se crea la interfaz de logica para Invitation Code Se crea prueba para Ver el codigo

---
+++ Stockapp/Stockapp.Logic/API/IInvitationCodeLogic.cs
@@ -0,0 +1,19 @@
+﻿using Stockapp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockapp.Logic.API
+{
+    public interface IInvitationCodeLogic
+    {
+        /// <summary>
+        /// Generates a random unique alphanumeric code.
+        /// </summary>
+        /// <param name="administator">The user that is requesting the code</param>
+        /// <returns></returns>
+        InvitationCode GenerateCode(User administator);
+    }
+}
---
+++ Stockapp/Stockapp.Logic/Implementation/InvitationCodeLogic.cs
@@ -0,0 +1,26 @@
+﻿using Stockapp.Logic.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stockapp.Data;
+using Stockapp.Data.Repository;
+
+namespace Stockapp.Logic.Implementation
+{
+    public class InvitationCodeLogic : IInvitationCodeLogic
+    {
+        private readonly IUnitOfWork UnitOfWork;
+
+        public InvitationCodeLogic(IUnitOfWork UnitOfWork)
+        {
+            this.UnitOfWork = UnitOfWork;
+        }
+
+        public InvitationCode GenerateCode(User administator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
